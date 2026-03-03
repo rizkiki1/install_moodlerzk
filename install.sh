474b8e563a0e62ffbf1850660e29430c81873dc8
rizkiki1 <erte2409@gmail.com>
2026-03-03 17:29:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ curl -fsSL https://packages.sury.org/php/apt.gpg | tee /usr/share/keyrings/php.g
 echo "deb [signed-by=/usr/share/keyrings/php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" \ | tee /etc/apt/sources.list.d/php.list
 apt install git unzip curl wget -y
 apt install -y ca-certificates apt-transport-https lsb-release curl
-apt install php8.2 libapache2-mod-php8.2 php8.2-{cli.mysql, xml,gd,intl,mbstring,curl,zip,soap,bcmath,opcache} -y
+apt install php8.2 libapache2-mod-php8.2 php8.2-{cli.mysql,xml,gd,intl,mbstring,curl,zip,soap,bcmath,opcache} -y
 apt install mariadb-server -y
+apt install dos2unix -y
